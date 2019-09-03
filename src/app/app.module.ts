72d3cbd04fd7463ab3ea55754fbb168d15cc8df7
rajeswaa <rajeswaa@tollgroup.com>
2019-09-03 11:29:28 +1000

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
+import { CustomersModule } from './customers/customers.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ProductModule,
+    CustomersModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
